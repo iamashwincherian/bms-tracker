c38f28a559bacced6b3dcd2d6dd2ba7ad3eacce8
Ashwin Cherian Joseph <iamashwincherian@gmail.com>
2026-04-14 15:59:44 +0530

    fix param type

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ async def get_theatre_names(
 async def search_shows(
     city: str,
     movie: str,
-    id=str,
+    movie_id: str,
     date: str,
     theatre: Optional[str] = None,
     send_email_to: Optional[str] = None
@@ -51,7 +51,7 @@ async def search_shows(
     - **date**: Date in YYYYMMDD format (e.g., 20250830)
     - **theatre**: Theatre name to filter (e.g., PVR, INOX) - optional
     """
-    bms = BMS(city, movie, date, theatre, movie_id=id)
+    bms = BMS(city, movie, date, theatre, movie_id)
     await bms.init()
     try:
         res = await bms.get_shows()
